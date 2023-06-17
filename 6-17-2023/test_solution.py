4895add4c89b9273af9b7bc412215955bd71301a
Johan Sandoval <jojo.han718@gmail.com>
2023-06-17 14:26:04 -0500

    Day 6 problem 6-17-23

---
+++ 6-17-2023/test_solution.py
@@ -0,0 +1,15 @@
+from solution import Solution
+import unittest
+from ast import literal_eval
+
+class TestSolution(unittest.TestCase):
+    def test_solution(self):
+        with open('6-17-2023/testcases.txt') as ifile: 
+            for iline in ifile:
+                with self.subTest(line=iline):
+                    iline = iline.strip("\n")
+                    lineList = iline.split(", ")
+                    self.assertEqual(Solution.productExceptSelf(self, literal_eval(lineList[0])), literal_eval(lineList[1]))
+
+if __name__ == '__main__':
+    unittest.main()
