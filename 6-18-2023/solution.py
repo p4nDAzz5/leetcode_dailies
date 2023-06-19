3d25d5a17860cb83133d1d0a2c14bdfa5f6e1be4
Johan Sandoval <jojo.han718@gmail.com>
2023-06-19 14:05:07 -0500

    Update day 7 problem with working solution

--- 6-18-2023/solution.py
+++ 6-18-2023/solution.py
@@ -1,21 +1,22 @@
-class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols = collections.defaultdict(set)
-        rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set)  # key = (r /3, c /3)
+class Solution(object):
+    def isValidSudoku(self, board):
+        from collections import defaultdict
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        n = 9
+        rowBoard = [[] for x in range(n)]
+        colBoard = [[] for x in range(n)]
+        boxBoard = defaultdict(set)
 
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] == ".":
-                    continue
-                if (
-                    board[r][c] in rows[r]
-                    or board[r][c] in cols[c]
-                    or board[r][c] in squares[(r // 3, c // 3)]
-                ):
+        for rows in range(len(board)):
+            for cols in range(len(board[rows])):
+                boxBoard[rows/3, cols/3] = set()
+                if (board[rows][cols] in colBoard[cols] or board[rows][cols] in rowBoard[rows] or board[rows][cols] in boxBoard[rows] or board[rows][cols] in boxBoard[rows/3, cols/3]) and board[rows][cols].isdigit():
                     return False
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r // 3, c // 3)].add(board[r][c])
+                colBoard[cols].append(board[rows][cols])
+                rowBoard[rows].append(board[rows][cols])
+                boxBoard[rows/3, cols/3].add(board[rows][cols])
 
         return True
